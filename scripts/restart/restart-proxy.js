4843fbaa8e026240b124cd1e849c11ba878842f6
qwertypnk <qwertypnk@protonmail.com>
2021-10-18 10:59:22 +0300

    deploy: >> 0.9.1 <<

--- scripts/restart/restart-proxy.js
+++ scripts/restart/restart-proxy.js
@@ -4,11 +4,6 @@ const { execSync } = require('child_process');
 
 const main = async () => {
   try {
-    try {
-      execSync('npm run start:proxy', { stdio: 'ignore' });
-    } catch (err) {
-      // do nothing
-    }
     execSync('nginx -s reload', { stdio: 'inherit' });
   } catch (err) {
     console.error(err);
